e591feadf3040b372a09df777a8aa57058cccc39
Aqazilbash <157313524+Aqazilbash@users.noreply.github.com>
2025-09-03 12:56:26 -0400

    summary

--- Bisection_method.m
+++ Bisection_method.m
@@ -1,30 +1,28 @@
-function [root] = Bisection_method(x_l, x_r)
+function [root] = Bisection_method(x_l, x_r, test_func)
+%Definition of the test function and its derivative
 while true
 x_mid = (x_l + x_r) / 2;
-if test_func01(x_l) == 0
+if test_func(x_l) == 0
     root = x_l;
     break
 end
-if test_func01(x_r) == 0
+if test_func(x_r) == 0
     root = x_r;
     break
 end 
-if test_func01(x_mid) == 0
+if test_func(x_mid) == 0
     root = x_mid;
     break
 end
-if (x_l < 0 & x_r < 0) || (x_l > 0 & x_r > 0)
+if (test_func(x_l) < 0 && test_func(x_r) < 0) || (test_func(x_l) > 0 && test_func(x_r) > 0)
     root = NaN;
     break
 end
-if (test_func01(x_l) > 0 && test_func01(x_mid) < 0) || (test_func01(x_l) < 0 && test_func01(x_mid) > 0)
+if (test_func(x_l) > 0 && test_func(x_mid) < 0) || (test_func(x_l) < 0 && test_func(x_mid) > 0)
     x_r = x_mid;
-else if (test_func01(x_mid) > 0 && test_func01(x_r) < 0) || (test_func01(x_mid) < 0 && test_func01(x_r) > 0)
+else if (test_func(x_mid) > 0 && test_func(x_r) < 0) || (test_func(x_mid) < 0 && test_func(x_r) > 0)
         x_l = x_mid;
 end 
-if test_func01(root) == 0
-    break
-end
 
 end
 
