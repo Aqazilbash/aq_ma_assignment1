d9136c74316116737ca4d83dd8989e9a6fb21c93
mayaadelman1 <madelman@olin.edu>
2025-09-08 18:59:31 -0400

    idk

--- Newtons_method.m
+++ Newtons_method.m
@@ -3,12 +3,14 @@ function [root] = Newtons_method(xn, A_thresh, B_thresh, fun)
 % Defines function and its derivative
 test_func = fun{1};
 test_derivative = fun{2};
+guesses = [];
 
 while true
 
   % Division safeguard
         if abs(test_func(xn)) < 1e-14
             root = xn;
+            guesses = [guesses, xn];
             return
         end
 
@@ -18,11 +20,13 @@ while true
         % Early termination
         if abs(xn1 - xn) < A_thresh || abs(test_func(xn)) < B_thresh
             root = xn1;
+            guesses = [guesses, xn1];
             return
         end
 
         % Update current guess
         xn = xn1;
+        guesses = [guesses, xn1];
 end
 
 end
